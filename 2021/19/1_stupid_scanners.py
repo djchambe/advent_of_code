ff772af1472641019d6159e231842df97b035a8e
DJC <djchambe@gmail.com>
2021-12-27 14:10:36 -0800

    AOC2021 | WIP day 19

---
+++ 2021/19/1_stupid_scanners.py
@@ -0,0 +1,186 @@
+from datetime import datetime
+from collections import deque
+from copy import deepcopy
+
+def shift_scanner_beacons(scanner):
+    adjustments = scanner.get('adjustments', {})
+    beacons = scanner['beacons']
+    if adjustments:
+        return get_shifted_beacons(beacons, [0, 0, 0], adjustments)
+    
+    return beacons
+
+def get_unique_beacons(scanners):
+    beacons = []
+    for scanner in scanners.values():
+        scanner_beacons = shift_scanner_beacons(scanner)
+        beacons.extend(scanner_beacons)
+
+    unique_beacons = set(beacons)
+
+    return unique_beacons
+
+def get_same_beacons_count(beacons_1, beacons_2):
+    same = 0
+    for beacon_1 in beacons_1:
+        for beacon_2 in beacons_2:
+            if beacon_1 == beacon_2:
+                same += 1
+                break
+    
+    return same
+
+def get_shifted_beacons(beacons, known_position, adjustments):
+    shifted_beacons = []
+    unknown_position = adjustments['position']
+    for beacon in beacons:
+        updated_beacon = []
+        for i in range(len(known_position)):
+            updated_beacon.append(unknown_position[i] - known_position[i] + beacon[i])
+        shifted_beacons.append(updated_beacon)
+
+    return shifted_beacons
+
+def get_unknown_beacons(unknown_beacons, adjustments):
+    unknown_beacons_adjusted = []
+    negations = adjustments['negations']
+    rotations = adjustments['rotations']
+    switch = adjustments['switch']
+    for beacon in unknown_beacons:
+        if negations:
+            for i in negations:
+                beacon[i] *= -1
+        if rotations:
+            beacon.rotate(rotations)
+        elif switch:
+            beacon = apply_switch(beacon, switch)
+        unknown_beacons_adjusted.append(beacon)
+
+    return unknown_beacons_adjusted
+
+def get_unknown_position(known_beacon, unknown_beacon, known_position):
+    position = []
+    for i in range(len(known_position)):
+        position.append(known_beacon[i] - unknown_beacon[i] + known_position[i])
+    
+    return position
+
+def apply_switch(beacon, switch):
+    temp = beacon[switch[0]]
+    beacon[switch[0]] = beacon[switch[1]]
+    beacon[switch[1]] = temp
+
+    return beacon
+
+def get_adjustments(
+        known_beacon, unknown_beacon, known_position, negation, alteration):
+    adjustments = {
+        'position': [],
+        'negations': [],
+        'rotations': 0,
+        'switch': []
+    }
+    unknown_beacon_copy = deepcopy(unknown_beacon)
+    binary_negation = bin(negation)[2:]
+    for index, digit in enumerate(binary_negation):
+        if digit == '1':
+            unknown_beacon_copy[index] *= -1
+            adjustments['negations'].append(index)
+    if alteration < 3:
+        unknown_beacon_copy.rotate(alteration)
+        adjustments['rotations'] = alteration
+    elif alteration == 3:
+        switch = [0, 1]
+        unknown_beacon_copy = apply_switch(unknown_beacon_copy, switch)
+        adjustments['switch'] = switch
+    elif alteration == 4:
+        switch = [0, 2]
+        unknown_beacon_copy = apply_switch(unknown_beacon_copy, switch)
+        adjustments['switch'] = switch
+    elif alteration == 5:
+        switch = [1, 2]
+        unknown_beacon_copy = apply_switch(unknown_beacon_copy, switch)
+        adjustments['switch'] = switch
+    adjustments['position'] = get_unknown_position(
+        known_beacon, unknown_beacon_copy, known_position)
+
+    return adjustments
+
+def try_to_find_scanner(scanners, known_scanner_number, unknown_scanner_number):
+    known_beacons = scanners[known_scanner_number]['beacons']
+    known_position = scanners[known_scanner_number]['position']
+    unknown_beacons = deepcopy(scanners[unknown_scanner_number]['beacons'])
+    for unknown_beacon in unknown_beacons:
+        for known_beacon in known_beacons:
+            for i in range(8):
+                for j in range(6):
+                    adjustments = get_adjustments(
+                        known_beacon, unknown_beacon, known_position, i, j)
+                    unknown_beacons_adjusted = get_unknown_beacons(
+                        unknown_beacons, adjustments)
+                    unknown_beacons_shifted = get_shifted_beacons(
+                        unknown_beacons_adjusted, known_position, adjustments)
+                    same_beacons_count = get_same_beacons_count(
+                        known_beacons, unknown_beacons_shifted)
+                    if same_beacons_count >= 12:
+                        scanners[unknown_scanner_number]['position'] = adjustments['position']
+                        scanners[unknown_scanner_number]['adjustments'] = adjustments
+                        scanners[unknown_scanner_number]['beacons'] = unknown_beacons_adjusted
+                        return scanners
+
+    return scanners
+
+def find_unknown_scanners(scanners, positions):
+    for position in positions:
+        known_scanner_number = position[0]
+        known_scanner_position = position[1]
+        for scanner_number, scanner_data in deepcopy(scanners).items():
+            if scanner_data['position'] is None:
+                scanners = try_to_find_scanner(scanners, known_scanner_number, scanner_number)
+    
+    return scanners
+
+def get_positions(scanners):
+    positions = []
+    for scanner_number, scanner_data in scanners.items():
+        scanner_position = scanner_data['position']
+        if scanner_position is not None:
+            positions.append([scanner_number, scanner_position])
+    
+    return positions
+
+def count_beacons(scanners):
+    positions = get_positions(scanners)
+    while len(positions) < len(scanners):
+        scanners = find_unknown_scanners(scanners, positions)
+        positions = get_positions(scanners)
+    
+    unique_beacons = get_unique_beacons(scanners)
+
+    return len(unique_beacons)
+
+def handler():
+    print(f'Start: {datetime.now()}')
+    scanners = {}
+    count = -1
+    with open('input.txt') as input_file:
+        for line in input_file:
+            clean_line = line.rstrip()
+            if 'scanner' in clean_line:
+                count += 1
+                scanners[count] = {}
+                scanners[count]['beacons'] = []
+                scanners[count]['position'] = None if count else [0, 0, 0]
+            elif clean_line:
+                beacon_coordinates = clean_line.split(',')
+                scanners[count]['beacons'].append(deque([int(coordinate) for coordinate in beacon_coordinates], maxlen=3))
+    
+    answer = count_beacons(scanners)
+    # answer = get_beacon_differences(scanners)[0]
+    # answer = scanners[1]
+    
+    print(f'The answer is {answer}')
+    print(f'End: {datetime.now()}')
+
+if __name__ == '__main__':
+    handler()
