39b88a7d2fc11588d4a6babaa8797638797b6944
DJC <djchambe@gmail.com>
2022-12-03 22:21:15 -0800

    AOC2022 | Add day 4

---
+++ 2022/04/1_full_overlaps.py
@@ -0,0 +1,37 @@
+from time import perf_counter
+
+
+def evaluate_assignments(pair_assignments):
+    if pair_assignments[0] > pair_assignments[2]:
+        if pair_assignments[1] <= pair_assignments[3]:
+            return 1
+        else:
+            return 0
+    elif pair_assignments[0] == pair_assignments[2]:
+        return 1
+    else:
+        if pair_assignments[1] >= pair_assignments[3]:
+            return 1
+        else:
+            return 0
+
+
+def handler():
+    start_time = perf_counter()
+    answer = 0
+    with open('input.txt') as input_file:
+        for line in input_file:
+            clean_line = line.strip()
+            temp_list = clean_line.split(',')
+            pair_assignments = []
+            for item in temp_list:
+                pair_assignments.extend(item.split('-'))
+            for index, item in enumerate(pair_assignments):
+                pair_assignments[index] = int(item)
+            answer += evaluate_assignments(pair_assignments)
+
+    print(f'The answer is {answer}')
+    print(f'Finished in {perf_counter() - start_time}')
+
+if __name__ == '__main__':
+    handler()
